9fb77ef73d135c5fa5a13a7ebe7fab4dff387685
Ingeniero Ricardo J Goez <125226899+ricardocv1n@users.noreply.github.com>
2025-02-03 14:22:15 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,166 @@
+let anniversary = "2024-07-15";
+let anniversaryDate = new Date(anniversary);
+let today = new Date();
+
+// Función para calcular la diferencia en años, meses y días
+function dateDiff(startDate, endDate) {
+    let years = endDate.getFullYear() - startDate.getFullYear();
+    let months = endDate.getMonth() - startDate.getMonth();
+    let days = endDate.getDate() - startDate.getDate();
+
+    // Si los días son negativos, resta un mes y ajusta los días
+    if (days < 0) {
+        months--;
+        let lastDayOfPreviousMonth = new Date(endDate.getFullYear(), endDate.getMonth(), 0).getDate();
+        days += lastDayOfPreviousMonth;
+    }
+
+    // Si los meses son negativos, resta un año y ajusta los meses
+    if (months < 0) {
+        years--;
+        months += 12;
+    }
+
+    return { years, months, days };
+}
+
+let diff = dateDiff(anniversaryDate, today);
+
+// Actualiza el contenido en la página con los resultados correctos
+document.getElementById("days").textContent = diff.days.toString();
+document.getElementById("months").textContent = diff.months.toString();
+document.getElementById("years").textContent = diff.years.toString();
+
+
+
+let musicPlayer = document.querySelector(".music-container");
+let togglePlayer = document.querySelector(".toggle-player");
+
+let trackInfo = document.querySelector(".track-info");
+let trackName = document.querySelector(".trackname");
+let trackArtist = document.querySelector(".trackartist");
+let trackNav = document.querySelector(".track-nav");
+
+let playPauseBtn = document.querySelector(".playpause-track");
+let nextBtn = document.querySelector(".next-track");
+let prevBtn = document.querySelector(".prev-track");
+
+let trackIndex = 0;
+let isPlaying = false;
+let isHidden = true;
+//Falso
+let currentTrack = document.createElement("audio");
+let soundBars = document.querySelector(".sound-bars");
+
+togglePlayer.addEventListener("click", function() {
+    isHidden = !isHidden;
+    if(isHidden){
+        musicPlayer.classList.remove("hide");
+        togglePlayer.innerHTML = '<img class="w-[98%]" src="icons/close.svg">';
+        trackInfo.style.transitionDelay = "0.4s";
+        trackNav.style.transitionDelay = "0.4s";
+    } else {
+        musicPlayer.classList.add("hide");
+        togglePlayer.innerHTML = '<img class="w-full" src="icons/plus.svg">';
+        trackInfo.style.transitionDelay = "0s";
+        trackNav.style.transitionDelay = "0s";
+    }
+});
+
+let soundBarsLottie = bodymovin.loadAnimation({
+    container: soundBars,
+    renderer: "svg",
+    loop: true,
+    autoPLay: false,
+    path: "https://lottie.host/9ec12a7e-e429-453a-9f22-a2af1dcb4dca/2zeuy4rwtP.json",
+});
+
+
+let trackList = [
+    //{
+    //    name: "Only",
+    //    artist: "Lee Hi",
+    //    path: "./music/only.mp3",
+    //},
+    //{
+    //    name: "Day & Night",
+    //    artist: "Jung Seung Hwan",
+    //    path: "./music/day and night.mp3",
+    //},
+    //{
+    //    name: "Love of my Life",
+    //    artist: "Reyne",
+    //    path: "./music/love of my life.mp3",
+    //},
+    //{
+    //    name: "The Only One",
+    //    artist: "Reyne",
+    //    path: "./music/the only one.mp3",
+    //},
+    {
+        name: "Perfect",
+        artist: "Ed Sheeran",
+        path: "./music/perfect.mp3",
+    },
+];
+
+// EVENT LISTENERS
+playPauseBtn.addEventListener("click", playPauseTrack);
+nextBtn.addEventListener("click", nextTrack);
+prevBtn.addEventListener("click", prevTrack);
+
+function loadTrack(trackIndex){
+    currentTrack.src = trackList[trackIndex].path;
+    trackName.textContent = trackList[trackIndex].name;
+    trackArtist.textContent = trackList[trackIndex].artist;
+    currentTrack.addEventListener("ended", nextTrack);
+    currentTrack.load();
+}
+
+loadTrack(trackIndex);
+
+function playPauseTrack(){
+    if(isPlaying == false){
+        playTrack();
+    }else{
+        pauseTrack();
+    }
+}
+
+function playTrack(){
+    currentTrack.play();
+    isPlaying = true;
+    playPauseBtn.innerHTML = '<img class="w-8" src="icons/pause.svg">';
+    soundBarsLottie.play();
+}
+
+function pauseTrack(){
+    currentTrack.pause();
+    isPlaying = false;
+    playPauseBtn.innerHTML = '<img class="w-8" src="icons/play.svg">';
+    soundBarsLottie.stop();
+}
+
+function nextTrack(){
+    if(trackIndex < trackList.length - 1){
+        trackIndex += 1;
+        loadTrack(trackIndex);
+        playTrack();
+    }else{
+        trackIndex = 0;
+        loadTrack(trackIndex);
+        playTrack();
+    } 
+}
+
+function prevTrack(){
+    if(trackIndex > 0){
+        trackIndex -= 1;
+        loadTrack(trackIndex);
+        playTrack();
+    }else{
+        trackIndex = trackList.length - 1;
+        loadTrack(trackIndex);
+        playTrack();
+    }
+}
